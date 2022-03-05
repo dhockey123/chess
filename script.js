58b6be86283a05f95e0f11fd1b1163348fafc664
djh <danjh234@outlook.com>
2022-03-05 07:55:47 +0000

    finished updating allowed chess moves; cleaned code

--- script.js
+++ script.js
@@ -1,71 +1,66 @@
-
 setBoard();
 
 // ------------> DRAG AND DROP <--------------
 
-var col, row, obj_id;
+let col; let row; let
+  obj_id;
 
-var legalMove = false;
-var attackWithPawn = false;
+let legalMove = false;
+const attackWithPawn = false;
 
-const element = document.querySelectorAll('img')
-const target = document.querySelectorAll('.grid')
+const element = document.querySelectorAll('img');
+const target = document.querySelectorAll('.grid');
 
-element.forEach(i => {
-	i.addEventListener('dragstart', dragstart)
-})
+element.forEach((i) => {
+  i.addEventListener('dragstart', dragstart);
+});
 
-function dragstart(i){
-	obj_id = Number(i.target.id)
-	targetedPiece = i.target
+function dragstart(i) {
+  obj_id = Number(i.target.id);
+  targetedPiece = i.target;
 }
 
-target.forEach(e => {
-	e.addEventListener('dragover', e => e.preventDefault())
-	e.addEventListener('drop', dragDrop)
-})
+target.forEach((e) => {
+  e.addEventListener('dragover', (e) => e.preventDefault());
+  e.addEventListener('drop', dragDrop);
+});
 
-function dragDrop(e){
-	e.preventDefault();
-	let isTargetOccupied = false;
-	
-	if(e.target.tagName === 'DIV'){
-		col = Number(e.target.getAttribute('col'))
-		row = Number(e.target.getAttribute('row'))
-	}
-	else if(e.target.tagName === 'IMG'){
-		col = Number(e.target.parentNode.getAttribute('col'))
-		row = Number(e.target.parentNode.getAttribute('row'))
-		isTargetOccupied = true;
-	} 	
+function dragDrop(e) {
+  e.preventDefault();
+  let isTargetOccupied = false;
 
-	makeMove(e, isTargetOccupied)
+  if (e.target.tagName === 'DIV') {
+    col = Number(e.target.getAttribute('col'));
+    row = Number(e.target.getAttribute('row'));
+  } else if (e.target.tagName === 'IMG') {
+    col = Number(e.target.parentNode.getAttribute('col'));
+    row = Number(e.target.parentNode.getAttribute('row'));
+    isTargetOccupied = true;
+  }
+
+  makeMove(e, isTargetOccupied);
 }
 
 // ------------------> DOM Move piece <---------------------
 
-function makeMove(e, isTargetOccupied){
-	if(isLegalMove(obj_id, col, row)){ // Decided in legalMoves.js
-		if(isTargetOccupied){
-			delete Pieces[e.target.id]
-			e.target.parentNode.appendChild(targetedPiece)
-			e.target.parentNode.removeChild(e.target.parentNode.firstElementChild)
-		}
-		else{
-			e.target.appendChild(targetedPiece);
-		}
-		update(col, row, obj_id)
-		legalMove = false;
-	}
-
+function makeMove(e, isTargetOccupied) {
+  if (isLegalMove(obj_id, col, row)) { // Decided in legalMoves.js
+    if (isTargetOccupied) {
+      delete Pieces[e.target.id];
+      e.target.parentNode.appendChild(targetedPiece);
+      e.target.parentNode.removeChild(e.target.parentNode.firstElementChild);
+    } else {
+      e.target.appendChild(targetedPiece);
+    }
+    update(col, row, obj_id);
+    legalMove = false;
+  }
 }
 
 // --------------> Rules for moving pieces <----------------
 
-function update(col, row, obj_id){
-	Pieces[obj_id].row = row;
-	Pieces[obj_id].col = col;
-	Pieces[obj_id].moved = true;
+function update(col, row, obj_id) {
+  Pieces[obj_id].row = row;
+  Pieces[obj_id].col = col;
+  Pieces[obj_id].moved = true;
 }
-
-
--- setBoard.js
+++ setBoard.js
@@ -4,8 +4,7 @@ function uniqid() {
 
 const Pieces = {};
 
-const board = document.querySelector('.board')
-
+const board = document.querySelector('.board');
 
 function createPiece(name, colour, value, col, row) {
   this.name = name;
@@ -14,30 +13,30 @@ function createPiece(name, colour, value, col, row) {
   this.col = col;
   this.row = row;
   this.alive = true;
-	this.moved = false;
+  this.moved = false;
 }
 
-function drawBoard(){
-	for(var row = 1; row <=8; row++){
-		for(var col = 1; col<=8; col++){
-			const div = document.createElement('div')
-			div.className = 'grid'
-			div.setAttribute('row', row)
-			div.setAttribute('col', col)
-			board.appendChild(div)
-		}
-	}
+function drawBoard() {
+  for (let row = 1; row <= 8; row++) {
+    for (let col = 1; col <= 8; col++) {
+      const div = document.createElement('div');
+      div.className = 'grid';
+      div.setAttribute('row', row);
+      div.setAttribute('col', col);
+      board.appendChild(div);
+    }
+  }
 }
 
-drawBoard()
+drawBoard();
 
-const grid = document.querySelectorAll('.grid')
+const grid = document.querySelectorAll('.grid');
 
 function setBoard() {
   grid.forEach((cell) => {
-    const row = Number(cell.getAttribute('row'))
+    const row = Number(cell.getAttribute('row'));
     const col = Number(cell.getAttribute('col'));
-    let id = uniqid();
+    const id = uniqid();
 
     if ((row % 2 === 0) && (col % 2 === 1)) {
       cell.style.background = '#769656';
@@ -48,31 +47,31 @@ function setBoard() {
     if ((row === 7)) {
       const pawnW = document.createElement('img');
       pawnW.src = './icons/pawnW.svg';
-      pawnW.setAttribute('id', id)
-			Pieces[id] = new createPiece('pawn', 'white', 1, col, row)
+      pawnW.setAttribute('id', id);
+      Pieces[id] = new createPiece('pawn', 'white', 1, col, row);
       cell.appendChild(pawnW);
     }
     if ((row === 2)) {
       const pawnB = document.createElement('img');
       pawnB.src = './icons/pawnB.svg';
-			pawnB.setAttribute('id', id)
-			Pieces[id] = new createPiece('pawn', 'black', 1, col, row)
+      pawnB.setAttribute('id', id);
+      Pieces[id] = new createPiece('pawn', 'black', 1, col, row);
       cell.appendChild(pawnB);
     }
 
     if (row === 1 && (col === 1 || col === 8)) {
       const rookB = document.createElement('img');
       rookB.src = './icons/rookB.svg';
-			rookB.setAttribute('id', id)
-			Pieces[id] = new createPiece('rook', 'black', 5, col, row)
+      rookB.setAttribute('id', id);
+      Pieces[id] = new createPiece('rook', 'black', 5, col, row);
 
       cell.appendChild(rookB);
     }
     if (row === 8 && (col === 1 || col === 8)) {
       const rookW = document.createElement('img');
       rookW.src = './icons/rookW.svg';
-			rookW.setAttribute('id', id)
-			Pieces[id] = new createPiece('rook', 'white', 5, col, row)
+      rookW.setAttribute('id', id);
+      Pieces[id] = new createPiece('rook', 'white', 5, col, row);
 
       cell.appendChild(rookW);
     }
@@ -80,16 +79,16 @@ function setBoard() {
     if (row === 1 && (col === 2 || col === 7)) {
       const knightB = document.createElement('img');
       knightB.src = './icons/knightB.svg';
-			knightB.setAttribute('id', id)
-			Pieces[id] = new createPiece('knight', 'black', 3, col, row)
+      knightB.setAttribute('id', id);
+      Pieces[id] = new createPiece('knight', 'black', 3, col, row);
 
       cell.appendChild(knightB);
     }
     if (row === 8 && (col === 2 || col === 7)) {
       const knightW = document.createElement('img');
       knightW.src = './icons/knightW.svg';
-			knightW.setAttribute('id', id)
-			Pieces[id] = new createPiece('knight', 'white', 3, col, row)
+      knightW.setAttribute('id', id);
+      Pieces[id] = new createPiece('knight', 'white', 3, col, row);
 
       cell.appendChild(knightW);
     }
@@ -97,46 +96,46 @@ function setBoard() {
     if (row === 1 && (col === 3 || col === 6)) {
       const bishopB = document.createElement('img');
       bishopB.src = './icons/bishopB.svg';
-			bishopB.setAttribute('id', id)
-			Pieces[id] = new createPiece('bishop', 'black', 3, col, row)
+      bishopB.setAttribute('id', id);
+      Pieces[id] = new createPiece('bishop', 'black', 3, col, row);
       cell.appendChild(bishopB);
     }
     if (row === 8 && (col === 3 || col === 6)) {
       const bishopW = document.createElement('img');
       bishopW.src = './icons/bishopW.svg';
-			bishopW.setAttribute('id', id)
-			Pieces[id] = new createPiece('bishop', 'white', 3, col, row)
-      
+      bishopW.setAttribute('id', id);
+      Pieces[id] = new createPiece('bishop', 'white', 3, col, row);
+
       cell.appendChild(bishopW);
     }
 
     if (row === 1 && (col === 4)) {
       const queenB = document.createElement('img');
       queenB.src = './icons/queenB.svg';
-			queenB.setAttribute('id', id)
-      Pieces[id] = new createPiece('queen', 'black', 9, col, row)
+      queenB.setAttribute('id', id);
+      Pieces[id] = new createPiece('queen', 'black', 9, col, row);
       cell.appendChild(queenB);
     }
     if (row === 8 && (col === 4)) {
       const queenW = document.createElement('img');
       queenW.src = './icons/queenW.svg';
-			queenW.setAttribute('id', id)
-      Pieces[id] = new createPiece('queen', 'white', 9, col, row)
+      queenW.setAttribute('id', id);
+      Pieces[id] = new createPiece('queen', 'white', 9, col, row);
       cell.appendChild(queenW);
     }
 
     if (row === 1 && (col === 5)) {
       const kingB = document.createElement('img');
       kingB.src = './icons/kingB.svg';
-			kingB.setAttribute('id', id)
-      Pieces[id] = new createPiece('king', 'black', 1000, col, row)
+      kingB.setAttribute('id', id);
+      Pieces[id] = new createPiece('king', 'black', 1000, col, row);
       cell.appendChild(kingB);
     }
     if (row === 8 && (col === 5)) {
       const kingW = document.createElement('img');
       kingW.src = './icons/kingW.svg';
-			kingW.setAttribute('id', id)
-      Pieces[id] = new createPiece('king', 'white', 1000, col, row)
+      kingW.setAttribute('id', id);
+      Pieces[id] = new createPiece('king', 'white', 1000, col, row);
       cell.appendChild(kingW);
     }
   });
